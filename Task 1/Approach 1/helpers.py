40d2173a3c3b156f7a43466cb296c142cd7df727
Sahil <108575832+SahilMangla14@users.noreply.github.com>
2023-12-12 23:14:24 +0530

    Updated helpers.py

--- Task 1/Approach 1/helpers.py
+++ Task 1/Approach 1/helpers.py
@@ -1,4 +1,39 @@
 from sklearn.metrics import mean_squared_error
+from torch.nn.utils.rnn import pad_sequence
+import numpy as np
+import pandas as pd
+import torch
+
+def get_bert_embeddings(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    inputs = inputs.to(device)
+    outputs = model(**inputs)
+    embeddings = outputs.last_hidden_state.cpu().mean(dim=1).squeeze().detach().numpy()
+    return embeddings
+
+def extract_topics(ner, text):
+    doc = ner(text)
+    entities = [ent.text for ent in doc.ents]
+
+    return entities
+
+def process_row(ner, row):
+    text = row['content']
+    ner_entities = extract_topics(ner, text)
+    return ner_entities
+
+def make_tweetDesc_likes(df):
+    tweet_desc = []
+    tweet_likes = []
+    for index, row in df.iterrows():
+        tweet_description = f"The tweet was posted by {row['inferred company']} on {row['date']} with tweet content keywords: {row['company_ner_entities']}. The username of the company is {row['username']}."
+        tweet_desc.append(tweet_description)
+        tweet_likes.append(row['likes'])
+    
+    tweet_desc = np.array(tweet_desc)
+    tweet_likes = np.array(tweet_likes)
+    return tweet_desc, tweet_likes
+
 
 def collate_fn(batch):
     input_ids, attention_mask, labels = zip(*batch)
@@ -46,8 +81,8 @@ def predict_model(model, test_dataloader, scaler_likes, device):
     y_pred_np = np.array(all_predictions)
 
     # Inverse transform using the scaler
-    y_test_inv = scaler_likes.inverse_transform(y_test_np)
-    y_pred_inv = scaler_likes.inverse_transform(y_pred_np)
+    y_test_inv = scaler_likes.inverse_transform(y_test_np.reshape(-1,1))
+    y_pred_inv = scaler_likes.inverse_transform(y_pred_np.reshape(-1,1))
 
     # Stack the arrays side by side
     comparison_result = np.column_stack((y_test_inv, y_pred_inv))
