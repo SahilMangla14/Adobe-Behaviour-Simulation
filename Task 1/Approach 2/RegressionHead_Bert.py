cd3c2f9f132002e5dd564e448b1c405163a5cb18
Sahil <108575832+SahilMangla14@users.noreply.github.com>
2023-12-13 00:33:34 +0530

    Update RegressionHead_Bert.py

--- Task 1/Approach 2/RegressionHead_Bert.py
+++ Task 1/Approach 2/RegressionHead_Bert.py
@@ -18,16 +18,16 @@ class RegressionHead(nn.Module):
 
 
 class BERTRegressionModel(nn.Module):
-    def __init__(self, bert_model, regression_head_date, regression_head_content, regression_head_username, regression_head_media, regression_head_company):
+    def __init__(self, bert_model, regression_head_date, regression_head_content, regression_head_username, regression_head_company):
         super(BERTRegressionModel, self).__init__()
         self.bert_model = bert_model
         self.regression_head_date = regression_head_date
         self.regression_head_content = regression_head_content
         self.regression_head_username = regression_head_username
-        self.regression_head_media = regression_head_media
+        # self.regression_head_media = regression_head_media
         self.regression_head_company = regression_head_company
 
-    def forward(self, input_ids_date, attention_mask_date, input_ids_content, attention_mask_content, input_ids_username, attention_mask_username, input_ids_media, attention_mask_media, input_ids_company, attention_mask_company):
+    def forward(self, input_ids_date, attention_mask_date, input_ids_content, attention_mask_content, input_ids_username, attention_mask_username, input_ids_company, attention_mask_company):
         # Embeddings for date
         outputs_date = self.bert_model(input_ids=input_ids_date, attention_mask=attention_mask_date)
         pooled_output_date = outputs_date['pooler_output']
@@ -44,9 +44,9 @@ class BERTRegressionModel(nn.Module):
         regression_output_username = self.regression_head_username(pooled_output_username)
 
         # Embeddings for media
-        outputs_media = self.bert_model(input_ids=input_ids_media, attention_mask=attention_mask_media)
-        pooled_output_media = outputs_media['pooler_output']
-        regression_output_media = self.regression_head_media(pooled_output_media)
+        # outputs_media = self.bert_model(input_ids=input_ids_media, attention_mask=attention_mask_media)
+        # pooled_output_media = outputs_media['pooler_output']
+        # regression_output_media = self.regression_head_media(pooled_output_media)
 
         # Embeddings for company
         outputs_company = self.bert_model(input_ids=input_ids_company, attention_mask=attention_mask_company)
@@ -54,7 +54,7 @@ class BERTRegressionModel(nn.Module):
         regression_output_company = self.regression_head_company(pooled_output_company)
 
         # Combine the outputs with learned weights
-        combined_output = regression_output_date + regression_output_content + regression_output_username + regression_output_media + regression_output_company
+        combined_output = regression_output_date + regression_output_content + regression_output_username + regression_output_company
 
         return combined_output
 
