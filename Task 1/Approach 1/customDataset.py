fd49c3f1dfc63ca71e69362ea403e33693196c05
Sahil <108575832+SahilMangla14@users.noreply.github.com>
2023-12-12 23:15:07 +0530

    Updatef customDataset.py

--- Task 1/Approach 1/customDataset.py
+++ Task 1/Approach 1/customDataset.py
@@ -4,9 +4,11 @@ from transformers import BertTokenizer
 import torch
 
 class CustomDataset(Dataset):
-    def __init__(self, tweets, likes):
+    def __init__(self, tweets, likes, tokenizer, bert_model):
         self.tweets = tweets
         self.likes = likes
+        self.tokenizer = tokenizer
+        self.bert_model = bert_model
 
     def __len__(self):
         return len(self.tweets)
@@ -16,11 +18,11 @@ class CustomDataset(Dataset):
         like = torch.tensor(self.likes[idx], dtype=torch.float32)  # Convert to PyTorch tensor
 
         # Tokenize and get BERT embeddings
-        inputs = tokenizer(tweet, return_tensors="pt", truncation=True, padding=True, max_length=128)
+        inputs = self.tokenizer(tweet, return_tensors="pt", truncation=True, padding=True, max_length=128)
         input_ids = inputs['input_ids'].squeeze()  # Remove the added batch dimension
         attention_mask = inputs['attention_mask'].squeeze()  # Remove the added batch dimension
 
-        outputs = bert_model(**inputs)
+        outputs = self.bert_model(**inputs)
         pooled_output = outputs['pooler_output'].detach().numpy()
 
         return input_ids, attention_mask, like
